8db896e7440c242b96c5abdc0c881612398ac4ab
Yash <yashchaurasiya448@gmail.com>
2025-02-26 18:41:52 +0530

    login_using_oAuth2

--- backend/config.py
+++ backend/config.py
@@ -10,4 +10,5 @@ COLLECTION_user="users"
 GEMINI_API_KEY = "AIzaSyB-c2992fxHj9ZJqC5dO6FJMKcFXOa9rOM"
 
 #google OAuth2 API configuration
-
+GOOGLE_CLIENT_ID = "972667841116-24klldin603alco5lnle36dauedcsfe4.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET = "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"
